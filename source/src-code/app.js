2a933d98bf8c15a28a7d317afed2b86bfad3b16c
Pratik Bhurewar <Pratik.Bhurewar@extentia.com>
2018-03-14 16:27:42 +0530

    -linting.
    Signed-off-by: Pratik Bhurewar <Pratik.Bhurewar@extentia.com>

--- source/src-code/app.js
+++ source/src-code/app.js
@@ -1,3 +1,9 @@
+window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction ||
+	window.msIDBTransaction || { READ_WRITE: 'readwrite' };
+	 // This line should only be needed if it is needed to support the object's constants for older browsers
+window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
+
 import C from './home/home-page';
 
 const c = new C();
